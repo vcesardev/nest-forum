14766a2dead89fb999261d90e1910f1a4637cc40
Vitor Cesar <vitor@fasters.com.br>
2023-07-11 15:39:55 -0300

    feat: changes made to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [UserModule, PrismaModule],
+  imports: [
+    forwardRef(() => UserModule),
+    PrismaModule,
+    forwardRef(() => AuthModule),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
