2791618189dd5889d48601941f2d8ffa1f019706
Vitor Cesar <vitor@fasters.com.br>
2023-07-12 12:40:22 -0300

    feat: add Post Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { PostModule } from './modules/posts/post.module';
 
 @Module({
   imports: [
     forwardRef(() => UserModule),
     PrismaModule,
     forwardRef(() => AuthModule),
+    forwardRef(() => PostModule),
   ],
   controllers: [AppController],
   providers: [AppService],
